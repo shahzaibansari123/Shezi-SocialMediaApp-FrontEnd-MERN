b1cc382984430761340dfe323d725a8c3c9860a4
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-03 15:52:08 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import logo from "./images/logo.png";
 import Form from "./components/Form/Form"
 import Posts from './components/Posts/Posts'
 import useStyles from './Styles'
+import {useDispatch} from 'react-redux'
+import {getPosts} from './actions/posts'
 
 const App = () => {
   const classes=useStyles()
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
+  
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import Post from './Post/Post'
-import useStyles from './Styles'
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./Styles";
+import { useSelector } from "react-redux";
+
 const Posts = () => {
-    const classes=useStyles()
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+  console.log(posts);
   return (
     <>
       <h1>posts</h1>
       <Post />
       <Post />
     </>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
