e608c12a3a73df64492f5983349c5c441fb6aae4
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-04 19:21:26 +0500

    work complete

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import useStyles from './Styles'
-const Form = () => {
-    const classes=useStyles()
-    return (
-    <div>
-      <h1>from</h1>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import useStyles from "./Styles";
+import FileBase from 'react-file-base64'
 
-export default Form
+const Form = () => {
+  const [postData,setPostData]=useState({ 
+    creator: '', 
+    title: '', 
+    message: '', 
+    tags: '', 
+    selectedFile: ''
+  })
+  const classes = useStyles();
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autocomplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=> setPostData({...postData, title: e.target.value})}/>
+        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=> setPostData({...postData, message: e.target.value})}/>
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData, tags: e.target.value})}/>      
+        <div className={classes.fileInput}></div>
+          
+      
+      
+      </form>
+    </Paper>
+  );
+};
+
+export default Form;
