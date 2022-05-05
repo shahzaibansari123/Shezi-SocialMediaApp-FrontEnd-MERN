f3a0455b8c6686bd1f135b603a0aa7e7b13caab1
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-05 21:55:10 +0500

    work complete

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import useStyles from './Styles'
-const Post = () => {
+import {Card, CardContent, CardActions, CardMedia, Button, Typography}from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
+import DeleteIcon from '@material-ui/icons/Delete'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import moment from 'moment'
+
+const Post = ({post}) => {
     const classes=useStyles()
   return (
-    <div>
-      <h1>post</h1>
-    </div>
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+      <div classname={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{color : 'white'}} size="small" onClick={()=>{}}><MoreHorizIcon fontSize="default"/></Button>
+      </div>
+      
+    </Card>
+    // <div>
+    //   <h1>post</h1>
+    // </div>
   )
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,17 +2,27 @@ import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./Styles";
 import { useSelector } from "react-redux";
+import {Grid, CircularProgress} from '@material-ui/core'
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  console.log(posts);
+  // console.log(posts);
   return (
-    <>
-      <h1>posts</h1>
-      <Post />
-      <Post />
-    </>
+    !posts.length ? <CircularProgress /> :(
+      <Gird className={classes.container} container alignItems="stretch" spacing={3} >
+        {posts.map((post)=>(
+          <Grid item key={post._id} xs={12} sm={6} >
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Gird>
+    )
+    // <>
+    //   <h1>posts</h1>
+    //   <Post />
+    //   <Post />
+    // </>
   );
 };
 
