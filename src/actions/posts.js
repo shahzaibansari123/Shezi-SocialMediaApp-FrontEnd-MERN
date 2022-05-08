dbac83a7e597bbb301a31762a5c4d16f3e84b27d
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-08 23:31:50 +0500

    work complete

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,12 +1,12 @@
+import {CREATE, UPDATE, DELETE, FETCH_ALL} from '../constants/actionTypes'
 import * as api from '../api'
-import {CREATE, UPDATE, DELETE, FETCH_ALL, LIKE} from '../constants/actionTypes'
 //Action Creators
 
 export const getPosts=()=> async (dispatch)=>{
     
     try {
         const {data}= await api.fetchPosts()
-        dispatch({type: 'FETCH_ALL', payload: data });
+        dispatch({type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error.message)
     }
@@ -20,7 +20,7 @@ export const createPost=(post) => async (dispatch)=>{
     
     try {
         const {data}= await api.createPost(post)
-        dispatch({type: 'CREATE', payload: data });
+        dispatch({type: CREATE, payload: data });
     } catch (error) {
         console.log(error.message)
     }
@@ -32,7 +32,7 @@ export const updatePost=(id, post) => async (dispatch)=>{
     
     try {
         const {data}= await api.updatePost(id,post)
-        dispatch({type: 'UPDATE', payload: data });
+        dispatch({type: UPDATE, payload: data });
     } catch (error) {
         console.log(error.message)
         // console.log(error) ye krne se zada info mil jati error k bare me
@@ -46,7 +46,7 @@ export const deletePost=(id) => async (dispatch)=>{
     
     try {
     await api.deletePost(id)
-        dispatch({type: 'DELETE', payload: id});
+        dispatch({type: DELETE, payload: id});
     } catch (error) {
         console.log(error)
         // console.log(error) ye krne se zada info mil jati error k bare me
@@ -60,7 +60,7 @@ export const likePost=(id) => async (dispatch)=>{
     
     try {
         const {data}= await api.likePost(id)
-        dispatch({type: 'UPDATE', payload: data });
+        dispatch({type: UPDATE, payload: data });
     } catch (error) {
         console.log(error)
         // console.log(error) ye krne se zada info mil jati error k bare me
