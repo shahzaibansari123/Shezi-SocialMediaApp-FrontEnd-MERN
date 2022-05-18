740344a6339f720cee1a5ef4be8288eaf743e336
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-18 18:24:04 +0500

    work complete

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,10 @@ const API = axios.create({baseURL: 'http://localhost:5000'})
 
 API.interceptors.use((req)=>{
     if(localStorage.getItem('Profile')){
-        req.headers.Authorization =
+        req.headers.Authorization= `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`;
     }
+
+    return req;
 })
 
 // const url="http://localhost:5000/posts"
