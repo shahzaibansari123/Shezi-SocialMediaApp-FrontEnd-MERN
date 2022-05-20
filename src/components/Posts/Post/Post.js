79678890169ec22a53a94b249adc6a18f668ab79
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-20 17:29:18 +0500

    work complete

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,63 +1,121 @@
-import React from 'react'
-import useStyles from './Styles'
-import {Card, CardContent, CardActions, CardMedia, Button, Typography}from '@material-ui/core'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
-import DeleteIcon from '@material-ui/icons/Delete'
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
-import moment from 'moment'
-import {useDispatch} from 'react-redux'
-import {deletePost, likePost} from '../../../actions/posts'
+import React from "react";
+import useStyles from "./Styles";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
+import { useDispatch } from "react-redux";
+import { deletePost, likePost } from "../../../actions/posts";
 
-const Post = ({post, setCurrentId}) => {
-    const classes=useStyles()
-    const dispatch = useDispatch()
+const Post = ({ post, setCurrentId }) => {
+  const user = JSON.parse(localStorage.getItem("Profile"));
+  const classes = useStyles();
 
-    const Likes = () => {
-      if (post.likes.length > 0) {
-        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
-          ? (
-            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
-          ) : (
-            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
-          );
-      }
-  
-      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
-    };
+  const dispatch = useDispatch();
 
-    
+  const Likes = () => {
+    if (post.likes.length > 0) {
+      return post.likes.find(
+        (like) => like === (user?.result?.googleId || user?.result?._id)
+      ) ? (
+        <>
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp;
+          {post.likes.length > 2
+            ? `You and ${post.likes.length - 1} others`
+            : `${post.likes.length} like${post.likes.length > 1 ? "s" : ""}`}
+        </>
+      ) : (
+        <>
+          <ThumbUpAltOutlined fontSize="small" />
+          &nbsp;{post.likes.length} {post.likes.length === 1 ? "Like" : "Likes"}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize="small" />
+        &nbsp;Like
+      </>
+    );
+  };
+
   return (
     <Card className={classes.card}>
-      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.name}</Typography>
-        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button 
-          style={{color : 'white'}} 
-          size="small" 
-          onClick={()=> setCurrentId(post._id)}>
-          <MoreHorizIcon fontSize="default"/>
+        <Button
+          style={{ color: "white" }}
+          size="small"
+          onClick={() => setCurrentId(post._id)}
+        >
+          <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
-      <div className={classes.details} >
-      <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=> `#${tag} `)}</Typography>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
       </div>
-      <Typography className={classes.title} variant="h5" gutterBottom >{post.title}</Typography>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
       <CardContent>
-      <Typography variant="body2" color="textSecondary" component='p' gutterBottom >{post.message}</Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          gutterBottom
+        >
+          {post.message}
+        </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" />&nbsp; Like &nbsp; {post.likeCount}</Button>
-        <Button size="small" color="primary" onClick={()=> dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" />Delete </Button>
-      </CardActions>
+        <Button
+          size="small"
+          color="primary"
+          disabled={!user?.result}
+          onClick={() => dispatch(likePost(post._id))}
+        >
+          {/* <ThumbUpAltIcon fontSize="small" />
+          &nbsp; Like &nbsp; {post.likeCount} */}
 
+          <Likes />
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
+          <DeleteIcon fontSize="small" />
+          Delete{" "}
+        </Button>
+      </CardActions>
     </Card>
     // <div>
     //   <h1>post</h1>
     // </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
