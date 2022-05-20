b0d7a88b6453c21ad2b0fa2074fdc0a8f51ce0a3
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-20 17:19:10 +0500

    work complete

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -11,6 +11,21 @@ import {deletePost, likePost} from '../../../actions/posts'
 const Post = ({post, setCurrentId}) => {
     const classes=useStyles()
     const dispatch = useDispatch()
+
+    const Likes = () => {
+      if (post.likes.length > 0) {
+        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+          ? (
+            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+          ) : (
+            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+          );
+      }
+  
+      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
+
+    
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
