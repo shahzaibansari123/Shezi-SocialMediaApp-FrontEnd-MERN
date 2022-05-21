d50ccc8d3ca403e443e8dfe28a26400ae8d63dc9
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-21 19:57:27 +0500

    work complete

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,11 @@
+import 'react' from 'react';
+import {Pagination, PaginationItem} from '@material-ui/lab'
+
+import useStyles from './Styles'
+
+
+const Paginate=()=>{
+     const classes=useStyles()
+
+     return
+}
