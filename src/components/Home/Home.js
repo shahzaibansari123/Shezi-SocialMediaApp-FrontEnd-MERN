03ebf5f6faa57df70a70dba700bdd17cfffb6c9b
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-22 07:50:08 +0500

    work complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ import Form from "../Form/Form";
 import { getPosts } from '../../actions/posts'
 import Pagination from '../Pagination'
 
-
+import useStyles from './Styles'
 
 function useQuery(){
   return new URLSearchParams(useLocation().search)
@@ -23,6 +23,8 @@ const Home = () => {
   const history = useHistory()
   const page=query.get('page') || 1;
   const searchQuery= query.get('searchQuery') 
+  const classes=useStyles()
+const [search,setSearch]=useState(' ')
 
   useEffect(() => {
     dispatch(getPosts());
@@ -32,8 +34,8 @@ const Home = () => {
     <Grow in>
       <Container maxWidth="xl">
         <Grid
-        className={classes.gridContainer}
           container
+          className={classes.gridContainer}
           // className={classes.mainContainer}
           justify="space-between"
           alignItems="stretch"
@@ -43,11 +45,11 @@ const Home = () => {
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppBar className={classes.appBarSearch} postion="static" color="inherit">
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
               <TextField 
               name="search"
               variant="outlined" 
-              label=" Search Memories"
+              label=" Search Posts"
               fullWidth
               value="TEST"
               onChange={()=>{}}
