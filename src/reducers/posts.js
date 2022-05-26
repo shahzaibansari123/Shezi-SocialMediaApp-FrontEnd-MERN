0dc02bcce9632e292be3d384b8525345c10c2896
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-26 21:10:34 +0500

    work complete

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,18 +1,34 @@
-import {CREATE, UPDATE, DELETE, FETCH_ALL, FETCH_BY_SEARCH} from '../constants/actionTypes'
-export default (posts = [], action) => {
+import {
+  CREATE,
+  UPDATE,
+  DELETE,
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+} from "../constants/actionTypes";
+export default (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
-      return action.payload;
+      return {
+        ...state,
+        posts: action.payload.data,
+        currentPage: action.payload.currentPage,
+        numberOfPages: action.payload.numberOfPages,
+      };
     case FETCH_BY_SEARCH:
-      return action.payload;
+      return {
+        ...state,
+        posts: action.payload,
+      };
     case CREATE:
-      return [...posts, action.payload];
+      return [...state, action.payload];
     case UPDATE:
-    // case LIKE: 
-      return posts.map((post)=> post._id === action.payload._id ? action.payload : post);
-      case DELETE: 
-      return posts.filter((post)=> post._id !== action.payload)
+      // case LIKE:
+      return state.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case DELETE:
+      return state.filter((post) => post._id !== action.payload);
     default:
-      return posts;
+      return state;
   }
 };
