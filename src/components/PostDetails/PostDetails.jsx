53b494e1c7bd89f5d8a140fbb8b3b7cd81e5ed08
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-27 17:43:05 +0500

    work complete

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Paper, Divider, CircularProgress, Typography} from '@material-ui/core'
+import {useDispatch, useSelector} from 'react-redux'
+import moment from 'moment'
+import {useParams, useHistory} from 'react-router-dom'
+import  useStyles from './Styles'
 
 const PostDetails = () => {
     console.log('Post Details')
