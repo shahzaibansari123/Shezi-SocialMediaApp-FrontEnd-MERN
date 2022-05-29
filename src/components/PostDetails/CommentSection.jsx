de255364d0426e922cb413503cec2ece25220b75
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-30 00:08:24 +0500

    work complete

--- src/components/PostDetails/CommentSection.jsx
+++ src/components/PostDetails/CommentSection.jsx
@@ -4,10 +4,19 @@ import {useDispatch } from 'react-redux'
 import useStyles from './Styles'
 
 const CommentSection = ({post}) => {
+    const classes=useStyles()
     console.log('post')
+    const [comments , setComment]=useState([1,2,3,4])
   return (
     <div>
-     <div >fgfg</div>
+     <div className={classes.commentsOuterContainer}>
+         <div className={classes.commentsInnerContainer}>
+             <Typography gutterBottom variant="h6">Comments</Typography>
+             {comments.map((c, i)=>(
+                 <Typography key={i} variant="subtitle1" gutterBottom>Comment {i}</Typography>
+             ))}
+         </div>
+     </div>
     </div>
   )
 }
