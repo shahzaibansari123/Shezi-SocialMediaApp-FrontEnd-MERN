3bdbd546557211cfdb4e877157e1586f685a87d1
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-30 00:12:57 +0500

    work complete

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -72,7 +72,7 @@ const PostDetails = () => {
             <Typography gutterBottom variant="subtitle2" >{name}</Typography>
             <Typography gutterBottom variant="subtitle2" >{message}</Typography>
             <Typography gutterBottom variant="subtitle1" >likes: {likes.length}</Typography>
-            <img src={selectedFile} width="200px" alt="post detail img"/>
+            <img src={selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} width="200px" height="100px" alt="post detail img"/>
             
           </div>
         ))}
