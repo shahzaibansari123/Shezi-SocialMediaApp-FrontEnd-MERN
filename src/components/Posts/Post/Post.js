4f8d517157a4212b06f7e8aa1419b424ba3f6ada
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-05-31 22:05:46 +0500

    work complete

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 import useStyles from "./Styles";
 import {
   Card,
@@ -15,7 +15,8 @@ import {
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+// import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import EditIcon from '@material-ui/icons/Edit'
 import moment from "moment";
 import { useDispatch } from "react-redux";
 import { deletePost, likePost } from "../../../actions/posts";
@@ -27,23 +28,41 @@ const Post = ({ post, setCurrentId }) => {
   const history = useHistory();
 
   const dispatch = useDispatch();
+  const [likes, setLikes] = useState(post?.likes)
+
+  const userId=user?.result?.googleId || user?.result?._id
+  const hasLikedPost=post.likes.find((like) => like === userId)
+
+
+  //here fixing the quick response on likes bu JS tip
+  //state me dalne se response quick hojata
+const handleLike = async () => { 
+  dispatch(likePost(post._id))
+  if(hasLikedPost){
+    setLikes(post.likes.filter((id)=> id !== userId))
+
+  }else{
+     setLikes([...post.likes, userId])
+  }
+  
+}
 
   const Likes = () => {
-    if (post.likes.length > 0) {
-      return post.likes.find(
-        (like) => like === (user?.result?.googleId || user?.result?._id)
+    if (likes.length > 0) {
+      return likes.find(
+        (like) => like === userId
       ) ? (
         <>
           <ThumbUpAltIcon fontSize="small" />
           &nbsp;
-          {post.likes.length > 2
-            ? `You and ${post.likes.length - 1} others`
-            : `${post.likes.length} like${post.likes.length > 1 ? "s" : ""}`}
+          {likes.length > 2
+            ? `You and ${likes.length - 1} others`
+            : `${likes.length} like${likes.length > 1 ? "s" : ""}`}
         </>
       ) : (
         <>
           <ThumbUpAltOutlined fontSize="small" />
-          &nbsp;{post.likes.length} {post.likes.length === 1 ? "Like" : "Likes"}
+          &nbsp;{likes.length} {likes.length === 1 ? "Like" : "Likes"}
         </>
       );
     }
@@ -83,7 +102,7 @@ const Post = ({ post, setCurrentId }) => {
             e.stopPropagation()
             setCurrentId(post._id)}}
           >
-          <MoreHorizIcon fontSize="default" />
+          <EditIcon fontSize="default" />
         </Button>
       </div>
       )}
@@ -114,7 +133,7 @@ const Post = ({ post, setCurrentId }) => {
           size="small"
           color="primary"
           disabled={!user?.result}
-          onClick={() => dispatch(likePost(post._id))}
+          onClick={handleLike}
         >
           {/* <ThumbUpAltIcon fontSize="small" />
           &nbsp; Like &nbsp; {post.likeCount} */}
